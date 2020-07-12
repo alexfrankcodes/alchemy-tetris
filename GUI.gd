b19c5c1df3c0fe752cdf1f2bfcce2cb56f9135c3
Alex Frank <alexfrankcodes@gmail.com>
2020-07-12 13:03:03 -0700

    Added About popup

--- GUI.gd
+++ GUI.gd
@@ -5,6 +5,8 @@ const CELL_BG1 = Color(0.1, 0.1, 0.1)
 var grid
 var next
 
+signal button_pressed(button_name)
+
 func _ready():
 	grid = find_node("Grid")
 	next = find_node("NextBlock")
@@ -38,3 +40,24 @@ func clear_cells(node, color):
 		cell.modulate = color
 		
 	pass
+
+
+# TODO: Handle button presses
+func _on_NewGame_button_down():
+	emit_signal("button_pressed", "New Game")
+	pass # Replace with function body.
+
+
+func _on_PauseToggle_button_down():
+	emit_signal("button_pressed", "Pause")
+	pass # Replace with function body.
+
+
+func _on_MusicToggle_button_down():
+	emit_signal("button_pressed", "Music")
+	pass # Replace with function body.
+
+func _on_About_button_down():
+	$AboutBox.popup_centered()
+	emit_signal("button_pressed", "About")
+	pass # Replace with function body.
